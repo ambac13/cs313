ebcf3b5f028c654349cdd439d0d49cd060d7ce58
ambac13 <acristner@yahoo.com>
2016-09-29 21:47:56 -0400

    updated index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,48 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>Greetings</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>       
+       <title> Homepage </title>  
+       <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
-        <h1>Hello World!</h1>
-        <div><?php echo 'Hello, PHP World'; ?></div>
+            <header>
+                <h1> Amber Martinez </h1>  
+            </header>
+            <nav>  
+                <ul> 
+                    <li><a class="active" href = "index.html">Homepage</a><li> 
+                    <li><a href = "about.html">About</a></li> 
+                    <li><a href = "assignments.html">Assignments</a></li> 
+                </ul>
+            </nav>
+        <br>
+        
+            <div>
+              <img src="Boat.jpg" alt="Boat" style="width:600px;height:400px;"> 
+              <blockquote>
+                Welcome to my CS313 Homepage! <br>
+                Here you will find all of my assignments for CS313 <br>
+                Go explore and have fun! 
+              </blockquote>  
+            </div>
+                <footer>      
+                    <div id="footer">
+                        <table>
+                            <tr>
+                                <th>Contact</th>                  
+                                <th>Assignments</th>
+                                <th>Social</th>
+                            </tr>
+                            <tr>
+                                <td>Amber Martinez <br> ambzmartinez@gmail.com</td>                   
+                                <td><a href = "assignments.html">Assignments</a></td>
+                                <td>Facebook <br> Instagram <br> Twitter </td>
+                            </tr>
+                        </table>
+                            <p id="copyright">&copy; Amber Martinez</p>
+                            <p id="back-top"><a href="#">Back to Top</a></p>
+                    </div>
+                </footer>
     </body>
 </html>
